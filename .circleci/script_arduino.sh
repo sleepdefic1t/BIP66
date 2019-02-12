c59a7feb85c047f0f7ae19c256170d5407e82905
Simon <sleepdefic1t@users.noreply.github.com>
2019-02-12 14:15:03 -0800

    fix: sketch path in ci script_arduino.sh

--- .circleci/script_arduino.sh
+++ .circleci/script_arduino.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/Arduino/libraries/BIP66/
 mv ~/project/* ~/Arduino/libraries/BIP66
 
-arduino-cli compile --output temp.bin -b esp32:esp32:esp32 $PWD/examples/BIP66/BIP66.ino --debug
+arduino-cli compile --output temp.bin -b esp32:esp32:esp32 ~/Arduino/libraries/BIP66/examples/BIP66/BIP66.ino --debug
