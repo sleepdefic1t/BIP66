c472de0c819b260886130a76834fa645dd72cf56
Simon <sleepdefic1t@users.noreply.github.com>
2019-11-27 15:26:46 -0800

    fix: replace <algorithm> with <cstring>

--- src/bip66.cpp
+++ src/bip66.cpp
@@ -30,7 +30,7 @@
 
 #include <array>
 #include <cstdint>
-#include <algorithm>
+#include <cstring>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Global Constants
